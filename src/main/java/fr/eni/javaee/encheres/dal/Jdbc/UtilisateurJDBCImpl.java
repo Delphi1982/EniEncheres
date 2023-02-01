aa594bcaa7d8cd1afee6c6617f4d9f8c13410571
Roxv93 <romain.andreo@hotmail.fr>
2023-02-01 15:39:20 +0100

    try UtilisateurJDBCimpl

--- src/main/java/fr/eni/javaee/encheres/dal/Jdbc/UtilisateurJDBCImpl.java
+++ src/main/java/fr/eni/javaee/encheres/dal/Jdbc/UtilisateurJDBCImpl.java
@@ -4,51 +4,78 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import fr.eni.javaee.encheres.BusinessException;
 import fr.eni.javaee.encheres.bo.Utilisateur;
 import fr.eni.javaee.encheres.dal.DAO.UtilisateurDAO;
+import fr.eni.javaee.encheres.dal.CodesResultatDAL;
+import fr.eni.javaee.encheres.dal.ConnectionProvider;
 
 public class UtilisateurJDBCImpl implements UtilisateurDAO {
-	private static final String SELECT_BY_PSEUDO = "select no_utilisateur, pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur from Utilisateurs where pseudo = ?;";
-	
+	private static final String SELECT_BY_PSEUDO = "select no_utilisateur,pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur "
+			+ "from Utilisateurs where pseudo = ?;";
+
+
+
 	
 private Connection conn;
 	
 	@Override
-	public void insert(Utilisateur user)throws BusinessException{
-	
-		try {
-			PreparedStatement ps = conn.prepareStatement
+	public Utilisateur getUtilisateurByPseudo (String pseudo)throws BusinessException{
+		if(user==null)
+		{
+			BusinessException businessException = new BusinessException();
+//			businessException.ajouterErreur(CodesResultatDAL.?);
+			throw businessException;
+		}
+		
+		try(Connection cnx = ConnectionProvider.getConnection())
+		{
+			try
+			{
+			 PreparedStatement pstmt;
+			 ResultSet rs;
+			 if(user.getPseudo()==null)
+			   { 			
+				 PreparedStatement ps = conn.prepareStatement
 				("INSERT INTO UTILISATEURS (pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur) "
-				+ "VALUES (?,?,?,?,?,?,?,?,?,?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
-			ps.setString(1, user.getPseudo());
-			ps.setString(2, user.getNom());
-			ps.setString(3, user.getPrenom());
-			ps.setString(4, user.getEmail());
-			ps.setString(5, user.getTelephone());
-			ps.setString(6, user.getRue());
-			ps.setInt(7, user.getCodePostal());
-			ps.setString(8, user.getVille());
-			ps.setString(9, user.getMotDePasse());
-			ps.setInt(10, user.getCredit());
-			ps.setString(11, user.getAdministrateur());
+				+ "VALUES (?,?,?,?,?,?,?,?,?,?,?)", PreparedStatement.RETURN_GENERATED_KEYS);
+				 ps.setString(1, user.getPseudo());
+				 ps.setString(2, user.getNom());
+				 ps.setString(3, user.getPrenom());
+				 ps.setString(4, user.getEmail());
+				 ps.setString(5, user.getTelephone());
+				 ps.setString(6, user.getRue());
+				 ps.setInt(7, user.getCodePostal());
+				 ps.setString(8, user.getVille());
+				 ps.setString(9, user.getMotDePasse());
+				 ps.setInt(10, user.getCredit());
+				 ps.setString(11, user.getAdministrateur());
+			 
+				 ps.executeUpdate();
+				 ResultSet rs = ps.getGeneratedKeys();
 			
-			ps.executeUpdate();
-			ResultSet rs = ps.getGeneratedKeys();
-			
-			if (rs.next()) {
+			if (rs.next()) 
+			{
 				user.setNoUtilisateur(rs.getInt(1));
 			}
+			rs.close();
+			ps.close();
+			} 
+			}
+			 catch (SQLException e) 
+		
+		{	e.printStackTrace();
+			cnx.rollback();
+			throw e;
+		}	
 			
-		} catch (SQLException e) {
-			e.printStackTrace();
+	  } finally 
 			
-		} finally {
-		}
-	}
 	
-	@Override //comment l'app recupere le noId ?
+	
+	@Override 
 	public void delete(int noId)throws BusinessException{
 		try {
 			PreparedStatement ps = conn.prepareStatement("DELETE FROM UTILISATEURS WHERE idArticle = ?");
